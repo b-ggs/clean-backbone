de961e4923d8fc9a67f6f6489abe3b4f8027418c
Boggs <hello@boggs.xyz>
2016-10-05 10:52:00 +0800

    Add index method in tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,9 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @tasks }
+    end
+  end
 end
